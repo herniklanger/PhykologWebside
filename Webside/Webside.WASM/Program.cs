0a7027145e8575c9fb250c4bac2f67a1c9a4b525
Henrik Langer Hallenberg Schou <henrik-langer@live.dk>
2022-05-18 14:29:53 +0200

    Addet UI prosjektet.

--- Webside/Webside.WASM/Program.cs
+++ Webside/Webside.WASM/Program.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Webside.WASM;
+using Webside;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+// builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
